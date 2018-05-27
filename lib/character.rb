8eb9e600ee3b9f8ae432ac632999a78995875217
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-27 15:42:12 +0100

    Add evasion attribute and default available skillpoints

--- lib/character.rb
+++ lib/character.rb
@@ -1,9 +1,15 @@
-DEFAULT_HEALTH = 10
+DEFAULT_HEALTH = 30
+DEFAULT_ATTACK = 0
+DEFAULT_EVASION = 0
+DEFAULT_SKILLPOINTS = 10
 
 class Character
-  attr_reader :name, :health
+  attr_reader :name, :health, :attack, :evasion, :available_skillpoints
   def initialize(name)
     @name = name
     @health = DEFAULT_HEALTH
+    @attack = DEFAULT_ATTACK
+    @evasion = DEFAULT_EVASION
+    @available_skillpoints = DEFAULT_SKILLPOINTS
   end
 end
--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -10,8 +10,26 @@ describe Character do
   end
 
   describe '#health' do
-    it 'Returns the health of the character' do
-      expect(character.health).to eq 10
+    it 'Returns the health stat of the character' do
+      expect(character.health).to eq 30
+    end
+  end
+
+  describe '#attack' do
+    it 'Returns the attack stat of the character' do
+      expect(character.attack).to eq 0
+    end
+  end
+
+  describe '#evasion' do
+    it 'Returns the evasion stat of the character' do
+      expect(character.evasion).to eq 0
+    end
+  end
+
+  describe '#available_skillpoints' do
+    it 'Returns the available skillpoints of the character' do
+      expect(character.available_skillpoints).to eq 10
     end
   end
 end
